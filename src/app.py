036c674f27c8d0d2f6858fc35607a2de1476e914
Fernando Mehmet Li <hli@fqm.ca>
2024-11-11 19:56:15 -0500

    assignment

--- src/app.py
+++ src/app.py
@@ -6,11 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-        <form action="/echo_user_input" method="POST">
+        <form action="/echo" method="POST">
             <input type="text" name="user_input">
             <button type="submit">Submit!</button>
         </form>
     '''
+@app.route("/echo",methods=['POST'])
+def echo():
+    input=request.form['user_input']
+    return f'''
+        Did you type {input}?
+    '''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
